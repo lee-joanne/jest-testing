7a4b239e01bd3805d1be3683936b50271f63e76b
Joanne <lee.joanne54@gmail.com>
2023-11-09 00:54:16 +0000

    Complete dom testing

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,14 +5,26 @@
 const expectExport = require("expect");
 const buttonClick = require("../button");
 
-beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>"
-});
+// beforeEach(() => {
+//     document.body.innerHTML = "<p id='par'></p>"
+// });
 // creates mock dom as cannot import HTML elements onto test cases
+
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+    // built in way to load html file
+})
 
 describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expectExport(document.getElementById("par").innerHTML).toEqual("You Clicked");
     });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    })
 });
